18794bb03b28ec2592763a56ba1ba9415a60eea0
doudou5246-glitch <doudou5246@gmail.com>
2026-04-19 21:58:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -170,19 +170,6 @@ app.get('/qrcode/:id', async (req, res) => {
   const baseUrl = process.env.PUBLIC_BASE_URL || `${req.protocol}://${req.get('host')}`;
 
   const url = `${baseUrl}/outil.html?id=${id}`;
-
-
-
-  res.send(`
-    <html>
-      <body style="text-align:center;font-family:Arial">
-        <h2>QR Code outil ${id}</h2>
-        <img src="${qr}" />
-        <p>${url}</p>
-      </body>
-    </html>
-  `);
-});
   
 });
 const PORT = process.env.PORT || 3000;
